6c6a67bd0f7df1cc1a89ee221fa253990775d2aa
Ritwatz Raj <ritwatz@gmail.com>
2023-09-12 23:32:15 +0530

    image processing on gpu

--- Object Detection/SpaceDrawing.py
+++ Object Detection/SpaceDrawing.py
@@ -5,50 +5,53 @@ import pickle
 
 
 cap = cv2.VideoCapture('easy1.mp4')
-drawing=False
-area_names=[]
+drawing = False
+area_names = []
 try:
-    with open("abcd","r") as f:
-            data=pickle.load(f)
-            polylines,area_names=data['polylines'],data['area_names']
+    with open("abcd", "rb") as f:
+        data = pickle.load(f)
+        polylines, area_names = data['polylines'], data['area_names']
 except:
-    polylines=[]
-points=[]
-current_name=" "
+    polylines = []
+points = []
+current_name = " "
 
 
-def draw(event,x,y,flags,param):
-    global points,drawing
-    drawing=True
-    if event==cv2.EVENT_LBUTTONDOWN:
-        points=[(x,y)]
-    elif event==cv2.EVENT_MOUSEMOVE:
+def draw(event, x, y, flags, param):
+    global points, drawing
+    drawing = True
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points = [(x, y)]
+    elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
-            points.append((x,y))
-    elif event==cv2.EVENT_LBUTTONUP:
+            points.append((x, y))
+    elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        current_name=input('area name:-')
+        current_name = input('area name:-')
         if current_name:
             area_names.append(current_name)
-            polylines.append(np.array(points,np.int32))
+            polylines.append(np.array(points, np.int32))
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         continue
-    frame=cv2.resize(frame,(1020,500))
-    for i,polyline in enumerate(polylines):
+    frame = cv2.resize(frame, (1020, 500))
+    for i, polyline in enumerate(polylines):
         print(i)
-        cv2.polylines(frame,[polyline],True,(0,0,255),2)
-        cvzone.putTextRect(frame,f'{area_names[i]}',tuple(polyline[0]),1,1)
+        cv2.polylines(frame, [polyline], True, (0, 0, 255), 2)
+        cvzone.putTextRect(frame, f'{area_names[i]}', tuple(polyline[0]), 1, 1)
     cv2.imshow('FRAME', frame)
-    cv2.setMouseCallback('FRAME',draw)
-    if cv2.waitKey(1) & 0xFF==ord('s'):
-        with open("abcd","wb") as f:
-            data={'polylines':polyline,'area_names':area_names}
-            pickle.dump(data,f)
-    if cv2.waitKey(1) & 0xFF==ord('d'):
+    cv2.setMouseCallback('FRAME', draw)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        with open("abcd", "wb") as f:
+            data = {'polylines': polyline, 'area_names': area_names}
+            pickle.dump(data, f)
+    if cv2.waitKey(1) & 0xFF == ord('d'):
         break
+
+print(polylines)
 cap.release()
 cv2.destroyAllWindows()
